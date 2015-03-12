15d61f9e69875aaf5662fb356eb1c8fc5695a57c
Phil Dier <phil@dier.us>
2015-03-12 01:01:09 -0500

    dry chef version

--- scripts/vagrant_bootstrap.sh
+++ scripts/vagrant_bootstrap.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -ex
 
+chefdk=chefdk_0.4.0-1_amd64.deb
+
 # install chef dev kit. (includes berkshelf, needed below)
-if [ ! -f /tmp/chefdk.deb ]; then
-	wget -qO /tmp/chefdk.deb https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.4.0-1_amd64.deb
-	dpkg -i /tmp/chefdk.deb
+if [ ! -f /tmp/$chefdk ]; then
+	wget -qO /tmp/$chefdk https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/$chefdk
+	dpkg -i /tmp/$chefdk
 fi
 
 apt-get -y install git
