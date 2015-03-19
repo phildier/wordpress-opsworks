4307132689a91f74d90da823dc0a739a6471ecc3
Phil Dier <phil@dier.us>
2015-03-18 23:28:50 -0500

    change cache location

--- scripts/vagrant_bootstrap.sh
+++ scripts/vagrant_bootstrap.sh
@@ -3,9 +3,9 @@
 chefdk=chefdk_0.4.0-1_amd64.deb
 
 # install chef dev kit. (includes berkshelf, needed below)
-if [ ! -f /tmp/$chefdk ]; then
-	wget -qO /tmp/$chefdk https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/$chefdk
-	dpkg -i /tmp/$chefdk
+if [ ! -f /vagrant/cache/$chefdk ]; then
+	wget -qO /vagrant/cache/$chefdk https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/$chefdk
+	dpkg -i /vagrant/cache/$chefdk
 fi
 
 apt-get -y install git
